e1e05c07ecd34ea7d8e3a702ce41a3ddac9fed94
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-13 19:40:11 +0600

    appointmentcard added

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,13 +1,31 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointments = ({ selectedDate }) => {
+  const [appointmentOptions, setAppointmentOptions] = useState([]);
+  useEffect(() => {
+    fetch("appointmentOptions.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAppointmentOptions(data);
+      });
+  }, []);
   return (
-    <div>
-      <p className="text-center my-16 text-secondary font-bold">
+    <section className="my-16">
+      <p className="text-center  text-secondary font-bold">
         Available Appointments on {format(selectedDate, "PP")}
       </p>
-    </div>
+      <div className="grid gap-4 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mt-10">
+        {appointmentOptions.map((option) => (
+          <AppointmentOption
+            key={option._id}
+            option={option}
+          ></AppointmentOption>
+        ))}
+      </div>
+    </section>
   );
 };
 
