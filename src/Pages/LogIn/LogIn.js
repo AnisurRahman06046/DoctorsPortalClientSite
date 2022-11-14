d7286d142b75e4dd0971d0f75530fba18fa56c20
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-14 16:31:02 +0600

    firebase config,login setup done

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
+  const { register, handleSubmit } = useForm();
+  const handleLogin = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h1>this is login</h1>
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h1 className="text-xl text-center">Log In</h1>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              {...register("email")}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password")}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <label className="label">
+              <span className="label-text">Forget Password ?</span>
+            </label>
+          </div>
+
+          <input
+            className="btn btn-accent w-full"
+            type="submit"
+            value="Log In"
+          />
+        </form>
+        <p>
+          New to Doctors Portal
+          <Link className="text-secondary" to="/signup">
+            Create New Account
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+      </div>
     </div>
   );
 };
